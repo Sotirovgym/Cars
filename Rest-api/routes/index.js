5d54a0d45ef9407eff1ffd837006237cd0842a38
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-15 11:28:40 +0200

    Add car router to the routes/index.js

--- Rest-api/routes/index.js
+++ Rest-api/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const users = require('./users');
 const test = require('./test');
+const cars = require('./cars');
 const { authController } = require('../controllers');
 
 router.post('/register', authController.register);
@@ -8,6 +9,7 @@ router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
 router.use('/users', users);
+router.use('/cars', cars);
 router.use('/test', test);
 
 module.exports = router;
