861667c74635a2bf5cbffca5da66748e691eb100
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-16 17:05:15 +0200

    Adding end points for edit, like and delete car.

--- Rest-api/routes/cars.js
+++ Rest-api/routes/cars.js
@@ -6,5 +6,8 @@ router.get('/', carController.getCars);
 router.post('/', auth(), carController.createCar);
 
 router.get("/:carId", carController.getCar);
+router.put("/:carId", auth(), carController.editCar);
+router.put("/:carId", auth(), carController.likeCar);
+router.delete("/:carId", auth(), carController.deleteCar);
 
 module.exports = router;
