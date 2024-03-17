809416677b071a7e12a27c2c65f1454167052892
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-17 18:58:22 +0200

    Create User type

---
+++ FrontEnd/src/types/User.ts
@@ -0,0 +1,11 @@
+export interface User{
+  _id: string,
+  cars: string[],
+  tel: string,
+  email: string,
+  username: string,
+  password: string,
+  created_at: string,
+  updatedAt: string,
+  __v: number
+}
