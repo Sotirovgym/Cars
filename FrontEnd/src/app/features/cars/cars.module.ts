c7e509df275ebf518f5cc67241c4e6a204e277ce
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-18 11:32:37 +0200

    Create cars module

---
+++ FrontEnd/src/app/features/cars/cars.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarListComponent } from './car-list/car-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarsModule { }
