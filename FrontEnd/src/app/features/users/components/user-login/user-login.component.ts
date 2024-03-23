bcd22e4cc19de169f998a4755276cb3868e0771e
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-23 21:25:25 +0200

    Implementing register and login

--- FrontEnd/src/app/features/users/components/user-login/user-login.component.ts
+++ FrontEnd/src/app/features/users/components/user-login/user-login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { username, password } = form.value;
+    this.userService.login(username, password).subscribe(() => {
+      this.router.navigate(['/cars']);
+    });
+  }
 }
--- FrontEnd/src/app/features/users/components/user-register/user-register.component.ts
+++ FrontEnd/src/app/features/users/components/user-register/user-register.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-register.component.css']
+  styleUrls: ['./user-register.component.css'],
 })
 export class UserRegisterComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  register(form: NgForm) {
+    const { username, email, password, tel } = form.value;
+    this.userService.register(username, email, password, tel).subscribe(() => {
+      this.router.navigate(['/cars']);
+    });
+  }
 }
