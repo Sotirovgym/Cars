56b0df00d8f518d999cd90e6b2b96907b8995155
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-26 21:14:59 +0200

    Implementing logout with http request to the server

--- FrontEnd/src/app/core/components/header/header.component.ts
+++ FrontEnd/src/app/core/components/header/header.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../../features/users/user.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router, private notifyService: NotificationService) {}
 
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn;
+  }
+
+  get username(): string {
+    return this.userService.username;
+  }
+
+  logout() {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.notifyService.showSuccess('Successfully Logged Out', 'Logout');
+        this.router.navigate(['/auth/login']);
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
