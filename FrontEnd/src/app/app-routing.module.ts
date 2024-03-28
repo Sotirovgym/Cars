fde2818fce3ba99970dd74a848ed36469b1c358f
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-28 19:59:41 +0200

    Lazy load users and cars modules

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { ErrorComponent } from './core/components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'cars',
+    loadChildren: () => import('./features/cars/cars.module').then((m) => m.CarsModule),
+  },
+  // { path: 'error', component: ErrorComponent },
+  // { path: '**', redirectTo: '/404' },
+  // { path: '404', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
