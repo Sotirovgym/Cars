102b9e135f49e7697c4bc51719f0458c19902ddd
Georgi Sotirov <sotirovgym@abv.bg>
2024-03-29 12:12:22 +0200

    Add error-field shared component

---
+++ FrontEnd/src/app/shared/components/error-field/error-field.component.ts
@@ -0,0 +1,16 @@
+import { style } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl, FormGroupDirective } from '@angular/forms';
+
+@Component({
+  selector: 'app-error-field',
+  templateUrl: './error-field.component.html',
+  styleUrls: ['./error-field.component.css'],
+  host: { style: 'color: red;' },
+})
+export class ErrorFieldComponent {
+  @Input() control!: FormControl | AbstractControl;
+  @Input() errorMessages!: Object;
+
+  constructor(public formDirective: FormGroupDirective) {}
+}
--- FrontEnd/src/app/shared/shared.module.ts
+++ FrontEnd/src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorFieldComponent } from './components/error-field/error-field.component';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorFieldComponent,
+    ValidationPipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ErrorFieldComponent
   ]
 })
 export class SharedModule { }
