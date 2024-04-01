a786bbfee808fe170a0e318c221aadd0ecc68298
Georgi Sotirov <sotirovgym@abv.bg>
2024-04-01 12:59:04 +0300

    Create auth guard

--- FrontEnd/src/app/features/cars/car-routing.module.ts
+++ FrontEnd/src/app/features/cars/car-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarDetailsComponent } from './car-details/car-details.component';
+import { authGuard } from 'src/app/core/guards/auth.guard';
+import { CarAddComponent } from './car-add/car-add.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: CarListComponent },
-  { path: 'details/:carId', component: CarDetailsComponent }
+  { path: 'details/:carId', component: CarDetailsComponent },
+  {
+    path: 'create',
+    component: CarAddComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'edit/:carId',
+    component: CarAddComponent,
+    canActivate: [authGuard],
+  },
 ];
 
 @NgModule({
