14f59c9b4212f0d92d074d633dd3cfe05d145b37
Georgi Sotirov <sotirovgym@abv.bg>
2024-04-01 13:08:06 +0300

    import necessary modules

--- FrontEnd/src/app/features/cars/cars.module.ts
+++ FrontEnd/src/app/features/cars/cars.module.ts
@@ -5,17 +5,23 @@ import { CarCardComponent } from './car-card/car-card.component';
 import { RouterModule } from '@angular/router';
 import { CarRoutingModule } from './car-routing.module';
 import { CarDetailsComponent } from './car-details/car-details.component';
+import { CarAddComponent } from './car-add/car-add.component';
+import { SharedModule } from "../../shared/shared.module";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    CarListComponent,
-    CarCardComponent,
-    CarDetailsComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    CarRoutingModule
-  ]
+    declarations: [
+        CarListComponent,
+        CarCardComponent,
+        CarDetailsComponent,
+        CarAddComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        CarRoutingModule,
+        SharedModule,
+        ReactiveFormsModule
+    ]
 })
 export class CarsModule { }
