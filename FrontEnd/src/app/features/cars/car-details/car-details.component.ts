e5ada0327c9b41929cc2c8d300f24a5144c183de
Georgi Sotirov <sotirovgym@abv.bg>
2024-04-01 13:07:01 +0300

    Changes to the car-details and implementing edit and delete buttons

--- FrontEnd/src/app/features/cars/car-details/car-details.component.ts
+++ FrontEnd/src/app/features/cars/car-details/car-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Car } from 'src/types/Car';
 import { CarService } from '../car.service';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-car-details',
@@ -13,16 +15,35 @@ export class CarDetailsComponent implements OnInit {
 
   constructor(
     private activeRoute: ActivatedRoute,
-    private carService: CarService
+    private carService: CarService,
+    private authService: AuthService,
+    private notifyService: NotificationService,
+    private router: Router
   ) {}
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
 
   ngOnInit(): void {
     const carId = this.activeRoute.snapshot.paramMap.get('carId');
 
     if (carId) {
-      this.carService.getCar(carId).subscribe((car) => {
+      this.carService.getCarById(carId).subscribe((car) => {
         this.car = car;
       });
     }
+  }
+
+  deleteCar(){
+    this.carService.delete(this.car?._id!).subscribe({
+      next: () => {
+        this.notifyService.showSuccess('Post was successfully deleted', '');
+        this.router.navigate(['/cars']);
+      },
+      error: (res) => {
+        this.notifyService.showError(res.error.message, '');
+      }
+    })
   }
 }
